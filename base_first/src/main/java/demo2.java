42e98f6e2800abd136e7a2e6ad4e806e81d8f339
xuanlccc@gmail.com <xuanlccc@gmail.com>
2023-02-11 04:17:14 +0800

    Create demo2.java

---
+++ base_first/src/main/java/demo2.java
@@ -0,0 +1,14 @@
+/**
+ * @author Linker
+ * @date 2/11/2023 4:07 AM
+ * @description：
+ */
+public class demo2 {
+    /**
+     * Java 和 C++ 的区别
+     * 1. C++有指针
+     * 2. Java类单继承（接口多继承），C++多重继承
+     * 3. 
+     */
+
+}
